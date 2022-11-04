138ece8557b028eabdc2674cacd20789c35c8b8d
borneobats@outlook.com <devp@vt.edu>
2022-11-03 21:16:51 -0400

    email nodemailer done

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,12 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { SMTPClient } from "emailjs";
+const nodemailer = require("nodemailer");
 import type { NextApiRequest, NextApiResponse } from "next";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.NEXT_PUBLIC_MAIL,
+    pass: "ieqicqvdgrfnxhig",
+  },
+});
 
 /**
  * Sends an email from company's gmail account, found in environment variables,
  * to the email mentioned in the request.
  *
- * req {
+ * req.body {
  *     email: email@gmail.com
  *     CRN1: 124654
  *     CRN2: 546343
@@ -17,33 +25,28 @@ import type { NextApiRequest, NextApiResponse } from "next";
  *     message: send mail
  * }
  */
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { email, CRN1, CRN2 } = req.body;
-  // console.log(process.env)
+  const mailOptions = {
+    from: process.env.NEXT_PUBLIC_MAIL,
+    to: email,
+    subject: "Match Found",
+    text: `A new match for request (${CRN1}, ${CRN2}) was found`,
+  };
 
-  const client = new SMTPClient({
-    user: process.env.MAIL,
-    password: process.env.MAIL_PASSWORD,
-    host: "smtp.gmail.com",
-    ssl: true,
-  });
-
-  try {
-    client.send(
-      {
-        text: `A new match for request (${CRN1}, ${CRN2}) was found`,
-        from: `${process.env.MAIL}`,
-        to: email,
-        subject: `A new match found`,
-      },
-      (err, message) => {
-        console.log(err || message);
+  await new Promise((rsv, rjt) => {
+    transporter.sendMail(
+      mailOptions,
+      function (error: any, info: { response: string }) {
+        if (error) {
+          res.status(552).json({ error: error });
+        } else {
+          res.status(200).json({ success: "Email Sent!" });
+        }
       }
     );
-  } catch (e) {
-    res.status(400).end(JSON.stringify({ message: "Error" }));
-    return;
-  }
-
-  res.status(200).end(JSON.stringify({ message: "Send Mail" }));
+  });
 }
