a51c8e554d9f45c96c5f52b1dce929a2899ce0e1
shashankdatta <shashankdatta@vt.edu>
2022-11-29 01:39:08 -0500

    organized files a little

--- pages/index.tsx
+++ pages/index.tsx
@@ -249,7 +249,25 @@ const Home = ({ feedbacks, matches, matchTimes, adding, dropping }: IProps) => {
     <div className={`md:w-[750px] w-full h-full p-2`}>
       <Head>
         <title>Swap</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
       </Head>
 
       <AppContext.Provider
